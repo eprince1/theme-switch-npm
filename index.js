d8c45fcc84d6ee4115eb576bbfdb411ac37e2c14
eprince1 <eitanaprince@gmail.com>
2020-07-09 23:27:36 -0400

    changes

--- index.js
+++ index.js
@@ -1,61 +1,125 @@
-const axios = require('axios');
+const firebase = require("firebase");
 
-// const url = "http://localhost:5000/theme-switch/us-central1/api";
-const url = "https://us-central1-theme-switch.cloudfunctions.net/api";
+var firebaseConfig = {
+  apiKey: "AIzaSyD2bmDDpXR1SKJupQznpavT6yY5efulJTU",
+  authDomain: "theme-switch.firebaseapp.com",
+  databaseURL: "https://theme-switch.firebaseio.com",
+  projectId: "theme-switch",
+  storageBucket: "theme-switch.appspot.com",
+  messagingSenderId: "531247626942",
+  appId: "1:531247626942:web:948bcdbfcb269d4fcb3cef",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+let oldPaletteId = "";
 
 function initialize() {
-    let style = document.createElement("style");
-    style.id = "theme-switch-style";
-    document.head.appendChild(style);
+  let style = document.createElement("style");
+  style.id = "theme-switch-style";
+  document.head.appendChild(style);
 
-    const loadedPaletteId = localStorage.getItem('theme-switch-loaded-palette-id');
+  const loadedPaletteId = localStorage.getItem(
+    "theme-switch-loaded-palette-id"
+  );
 
-    if (loadedPaletteId) {
-        loadPalette(loadedPaletteId);
-    }
+  if (loadedPaletteId) {
+    loadPalette(loadedPaletteId);
+  }
 }
 
 function uninitialize() {
-    const style = document.getElementById("theme-switch-style");
-    style.innerHTML = '';
-    localStorage.setItem('theme-switch-loaded-palette-id', '');
+  const style = document.getElementById("theme-switch-style");
+  style.innerHTML = "";
+  localStorage.setItem("theme-switch-loaded-palette-id", "");
 }
 
 async function loadPalette(paletteId) {
-    const data = await axios.get(`${url}/palette?paletteId=${paletteId}`);
-    const palette = data.data;
+  if (oldPaletteId) {
+    database.ref(`palettes/${oldPaletteId}`).off();
+  }
 
-    if (!palette["palette"]) {
-        console.log("Palette not found");
-        return;
+  oldPaletteId = paletteId;
+
+  database.ref(`palettes/${paletteId}`).on("value", (snapshot) => {
+    const palette = snapshot.val();
+
+    if (!palette.palette) {
+      console.log("Palette not found");
+      return;
     }
 
     const style = document.getElementById("theme-switch-style");
 
-    style.innerHTML = '';
+    style.innerHTML = "";
 
-    palette["palette"].forEach(k => {
-        if (k['hover']) {
+    palette.palette.forEach((k) => {
+      const hover = k["hover"] ? ":hover" : "";
+
+      switch (k["type"]) {
+        case "border-color": {
+          const borderOptions = k["borderOptions"];
+
+          if (borderOptions) {
+            const attrs = "";
+            Object.keys(borderOptions).forEach((border) => {
+              if (borderOptions[border]) {
+                attrs += `${border}: ${k["hex"]};`;
+              }
+            });
+
             style.innerHTML += `
-                .${k['name']}:hover {
-                    ${k['type']}: ${k['hex']}
-                }
+              .${k["name"]}${hover} {
+                ${attrs}
+              }
             `;
-        } else {
+          } else {
             style.innerHTML += `
-                .${k['name']} {
-                    ${k['type']}: ${k['hex']}
-                }
+              .${k["name"]}${hover} {
+                border-color: none;
+              }
             `;
+          }
+
+          break;
         }
+        case "box-shadow": {
+          const shadowOptions = k["boxShadowOptions"];
 
+          if (shadowOptions) {
+            style.innerHTML += `
+              .${k["name"]}${hover} {
+                box-shadow: ${shadowOptions["hL"]}px ${shadowOptions["vL"]}px ${shadowOptions["blur"]}px ${shadowOptions["spread"]}px ${k["hex"]};
+              }
+            `;
+          } else {
+            style.innerHTML += `
+              .${k["name"]}${hover} {
+                box-shadow: 10px 10px 5px 0px ${k["hex"]};
+              }
+            `;
+          }
+
+          break;
+        }
+        default: {
+          style.innerHTML += `
+          .${k["name"]}${hover} {
+            ${k["type"]}: ${k["hex"]};
+          }
+        `;
+        }
+      }
     });
 
-    localStorage.setItem('theme-switch-loaded-palette-id', paletteId);
+    localStorage.setItem("theme-switch-loaded-palette-id", paletteId);
+  });
 }
 
 module.exports = {
-    initialize,
-    uninitialize,
-    loadPalette
+  initialize,
+  uninitialize,
+  loadPalette,
 };
